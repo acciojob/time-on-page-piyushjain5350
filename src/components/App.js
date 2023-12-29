e0af89d7cb7acb333a6b44dcd93bf80233c29696
Piyush Jain <piyushjain5350@gmail.com>
2023-12-29 21:27:33 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-
-import React from "react";
+import React ,{useEffect, useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [time,setTime]=useState(0);
+  
+  useEffect(()=>{
+   setTimeout(()=>{
+    setTime(time+1);  
+   },1000); 
+  })
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>You've been on this page for {time} seconds.</p>
+        
     </div>
   )
 }
